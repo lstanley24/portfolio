1eafc2d2cb021f26ce2d59c3d213f5d25b352397
Laura Stanley <laurastanley24@gmail.com>
2020-12-14 14:46:49 -0500

    initial commit :sparkles:

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const welcome = document.querySelector(".welcome");
+
+document.addEventListener("DOMContentLoaded",(event)=> {
+  setTimeout(()=>{
+    welcome.classList.add("display-none");
+
+
+  },3500)
+
+})
